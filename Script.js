377b7152c0525c3ab44eedb4523e870b444ef2ae
VIPIN KUSHWAHA <102202599+kushwahavipin@users.noreply.github.com>
2024-04-30 15:25:48 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,4 @@
+// Q1 - Square each element of the array using map function with an arrow function
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map(num => num * num);
+console.log(squaredNumbers);
